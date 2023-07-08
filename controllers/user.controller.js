675b636f7ba36d7f25632c46264cf67682b88371
Md. Shohanur Rahman <shohan.sub.56@gmail.com>
2023-07-08 09:24:52 +0600

    get all users updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -41,7 +41,7 @@ const registerUser = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
   try {
-    const users = await User.find({});
+    const users = await User.find({}).sort({ createdAt: -1 });
     res.status(200).json(users);
   } catch (error) {
     res.status(400).json({ error: error.message });
